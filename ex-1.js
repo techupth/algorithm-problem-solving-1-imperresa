0e1e2a6b4d60ee7de236bf14b2d2e7f6b73ec539
nueng <wongsaokrn.kump@gmail.com>
2024-07-10 14:17:07 +0700

    feat:answer

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,16 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+	let total = []
+	for(let i =0 ;i<numbers.length;i++){
+		for(let j =i+1;j<numbers.length;j++){
+			console.log(numbers[i],numbers[j]);
+			if(numbers[i]+numbers[j] === target){
+				total.push([i,j])
+			}
+		}
+		
+	}
+	return total
 };
+let numbers =  [2,7, 11, 15 ]
+let result = twoSum(numbers,9)
+console.log(result);
